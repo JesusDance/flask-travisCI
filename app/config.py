68c6730a002dfddbf5ecbf4b00987f9cdd7b4c43
Jesus <jiesusdance@gmail.com>
2026-03-02 09:43:00 +0200

    Prepare for deploy

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
+
+
 class Config:
     TESTING = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///expenses.sqlite3"
     JWT_SECRET_KEY = "jwt_super_secret_key"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
 
 class TestingConfig(Config):
